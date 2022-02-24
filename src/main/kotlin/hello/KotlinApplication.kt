353530a82db92e263f5b8bad6e5f1a460cb47e30
Fung Yam <fung.yam@gtomato.com>
2022-02-24 12:53:44 +0800

    Add self.getAction(target)

--- src/main/kotlin/hello/KotlinApplication.kt
+++ src/main/kotlin/hello/KotlinApplication.kt
@@ -21,31 +21,91 @@ class KotlinApplication {
 
         POST("/**", accept(APPLICATION_JSON)) { request ->
             request.bodyToMono(ArenaUpdate::class.java).flatMap { arenaUpdate ->
-                val self = arenaUpdate.arena.state[arenaUpdate._links.self.href]
-                    ?: return@flatMap ServerResponse.ok().body(Mono.just(listOf("F", "R", "L", "T").random()))
+                val self = arenaUpdate.arena.state[arenaUpdate._links.self.href] ?: return@flatMap randomAction
                 val others = arenaUpdate.arena.state - arenaUpdate._links.self.href
-                val (targetName, target) = others.entries.sortedBy { (key, player) ->
-                    abs(self.x - player.x) + abs(self.x - player.y)
-                }.first { !it.value.wasHit }
+                val (targetName, target) = others.entries.sortedBy { (_, player) ->
+                    self.absDelta(player)
+                }.firstOrNull { !it.value.wasHit } ?: return@flatMap randomAction
 
-                println("""
+                println(
+                    """
                     self = ${self.x}, ${self.y}
                     target = $targetName
-                """.trimIndent())
+                """.trimIndent()
+                )
+                self.getAction(target)
 
-                ServerResponse.ok().body(Mono.just(listOf("F", "R", "L", "T").random()))
+                ServerResponse.ok().body(Mono.just(self.getAction(target)))
             }
         }
     }
 }
 
+val randomAction get() = ServerResponse.ok().body(Mono.just(listOf("F", "R", "L", "T").random()))
+
+fun PlayerState.absDelta(target: PlayerState) = abs(dx(target)) + abs(dy(target))
+fun PlayerState.dx(target: PlayerState) = target.x - x
+fun PlayerState.dy(target: PlayerState) = target.y - y
+
+fun PlayerState.getAction(target: PlayerState): Action {
+    val dx = dx(target)
+    val dy = dy(target)
+    val absDelta = absDelta(target)
+    val dir = when {
+        dx > 0 -> Direction.E
+        dx < 0 -> Direction.W
+        dy > 0 -> Direction.S
+        dy < 0 -> Direction.N
+        else -> Direction.E // never
+    }
+    if (absDelta == 1) Action.T else Action.F
+    return turnToOrElse(
+        dir,
+        if (absDelta == 1) Action.T else Action.F
+    )
+}
+
+fun PlayerState.turnToOrElse(dir: Direction, action: Action) = when (direction) {
+    Direction.N -> when (dir) {
+        Direction.W -> Action.R
+        Direction.S -> Action.R
+        Direction.E -> Action.L
+        else -> action
+    }
+    Direction.W -> when (dir) {
+        Direction.N -> Action.L
+        Direction.S -> Action.R
+        Direction.E -> Action.R
+        else -> action
+    }
+    Direction.S -> when (dir) {
+        Direction.N -> Action.R
+        Direction.W -> Action.L
+        Direction.E -> Action.R
+        else -> action
+    }
+    Direction.E -> when (dir) {
+        Direction.N -> Action.R
+        Direction.W -> Action.R
+        Direction.S -> Action.L
+        else -> action
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<KotlinApplication>(*args)
 }
 
 data class ArenaUpdate(val _links: Links, val arena: Arena)
-data class PlayerState(val x: Int, val y: Int, val direction: String, val score: Int, val wasHit: Boolean)
+data class PlayerState(val x: Int, val y: Int, val direction: Direction, val score: Int, val wasHit: Boolean)
 data class Links(val self: Self)
 data class Self(val href: String)
 data class Arena(val dims: List<Int>, val state: Map<String, PlayerState>)
+
+enum class Direction {
+    N, W, S, E
+}
+
+enum class Action {
+    F, R, L, T
+}
